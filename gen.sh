28adfc6c520d802eb59d8d5bf12df94e2227350d
Elan Ruusamäe <glen@delfi.ee>
2019-02-08 13:24:42 +0200

    init composer if needed

--- gen.sh
+++ gen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -xeu
 
+test -f vendor/autoload.php || composer install --no-dev
+
 # PHP Fatal error:  Uncaught exception 'Doctrine\ORM\Mapping\MappingException' with message 'Table round_robin_user has no primary key. Doctrine does not support reverse engineering from tables that don't have a primary key.' in /home/glen/scm/eventum/doctrine2/vendor/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DatabaseDriver.php:288
 #mysql -s -e 'drop table if exists issue_association' e
 mysql -s -e 'drop table if exists reminder_action_list' e
