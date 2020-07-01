e9dbe5e759bda73c7c243fffb795b72015b74525
Elan Ruusamäe <glen@pld-linux.org>
2020-07-01 08:03:03 +0300

    Apply psr12
    $ vendor/bin/ecs check src --set psr12 --fix

--- src/Mail/MailMessage.php
+++ src/Mail/MailMessage.php
@@ -642,7 +642,8 @@ class MailMessage extends Message
         // process patterns
         $array = $headers->toArray();
         array_walk(
-            $array, function ($value, $name) use ($headers): void {
+            $array,
+            function ($value, $name) use ($headers): void {
                 if (preg_match('/^resent.*/i', $name)) {
                     $headers->removeHeader($name);
                 }
